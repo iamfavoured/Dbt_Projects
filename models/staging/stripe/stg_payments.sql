3fe33c58f860418921aca56ee7a9fbffddedd000
Favour Ikebukwu <ikebukwuf@gmail.com>
2024-01-17 14:17:54 +0000

    refresh

--- models/staging/stripe/stg_payments.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  id AS payment_id,
-  orderid AS order_id,
-  paymentmethod AS payment_method,
-  status,
-  amount/100 AS amount,
-  created AS created_at
-FROM
-  {{ source ('stripe', 'stripe_payments')}}
